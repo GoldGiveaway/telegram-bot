024f609ca511a80c69815d65d66191370f95e817
What-XD <101012824+What-XD@users.noreply.github.com>
2025-01-06 15:53:37 +0500

    Fix date

--- services/date.py
+++ services/date.py
@@ -11,4 +11,4 @@ def date_to_string(date) -> str:
     return date.strftime('%H:%M %d.%m.%Y')
 
 def string_to_date(date_str) -> datetime:
-    return datetime.strptime(date_str, '%H:%M %d.%m.%Y')
+    return datetime.strptime(date_str, '%H:%M %d.%m.%Y').astimezone(pytz.utc)
