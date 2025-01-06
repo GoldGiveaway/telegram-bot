9f52d3b9e0e8fffe63c0c03f1338a4a9d04f6de8
What-XD <101012824+What-XD@users.noreply.github.com>
2025-01-06 13:59:33 +0500

    Fix

--- interface/user.py
+++ interface/user.py
@@ -7,6 +7,6 @@ class IUser(BaseModel):
     user_id: int
     created_at: datetime = Field(default_factory=date.now_datetime)
     username: Optional[str]
-    last_name: str
-    first_name: str
+    last_name: Optional[str]
+    first_name: Optional[str]
 
